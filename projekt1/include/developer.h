dce85f107a70da0d8a7db889e1e05c873428716d
Yannick502 <schmitt.yannic-it24@it.dhbw-ravensburg.de>
2025-03-13 09:20:22 +0100

    Update developer.h
    Developer.h ist fertig

---
+++ projekt1/include/developer.h
@@ -0,0 +1,14 @@
+#ifndef DEVELOPER_H
+#define DEVELOPER_H                                                              //defines DEVELOPER_H if it isn`t already defined
+
+#include <stdio.h>
+
+typedef struct{                                                                  //defines the structer Developer
+    char name[50];                                                               //defines how much storage space the array of the name and alias has 
+    char alias[50];
+} Developer;
+
+void initDeveloper(Developer *developer, const char *name, const char *alias);  //declares the initialisation of developer with two pointers on the arrays name and alias
+void printDeveloper(const Developer *developer);                                //declares the function print developer
+
+#endif                                                                          //ends definition
