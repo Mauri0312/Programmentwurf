1b40f4ebbdba5db45ed178108174e6f12053e94e
Mauri0312 <manz.maurice-it24@it.dhbw-ravensburg.de>
2025-03-13 08:48:57 +0100

    Finished the work on our c files

--- projekt1/src/developer.c
+++ projekt1/src/developer.c
@@ -1,1 +1,11 @@
 #include "developer.h"
+#include <string.h>                                                                             
+
+void initDeveloper(Developer *developer, const char *name, const char *alias) {                 //Initialisation of developer-structure with name and alias
+    strcpy(developer->name, name);                                                              // Copies the content of 'name' into the 'name' field of the 'developer' structure              
+    strcpy(developer->alias, alias);                                                            // Copies the content of 'alias' into the 'alias' field of the 'developer' structure            
+}
+
+void printDeveloper(const Developer *developer) {
+    printf("Name: %s, Alias: %s\n", developer->name, developer->alias);                         //Prints the name and alias of the developer
+}
--- projekt1/src/developer_group.c
+++ projekt1/src/developer_group.c
@@ -1,1 +1,31 @@
 #include "developer_group.h"
+#include <stdio.h>
+#include <string.h>
+
+void initDeveloperGroup(DeveloperGroup *group, const char *logo) {                  //Initialisation of the DeveloperGroup-structure with logo
+    group->numDevelopers = 0;                                                       //Puts the number of developers in the group to zero
+    strcpy(group->logo, logo);                                                      //Copies the content of 'logo' into the 'logo' field of the 'group' structure
+}
+
+void addDeveloper(DeveloperGroup *group, const Developer *developer) {              //Initialisation of a function to adds a developer to the group
+    if (group->numDevelopers < 10) {                                                //If the number of developers in the group is less than 10
+        group->developers[group->numDevelopers] = *developer;                       //The developer is added to the group
+        group->numDevelopers++;                                                     //The number of developers in the group is increased by one
+    }
+}
+
+void printGroupLogo(const DeveloperGroup *group) {                                  //Initialisation of a function to print the group logo
+    printf("Group Logo: %s\n", group->logo);                                        //Prints the group logo
+}
+
+void printGroup(const DeveloperGroup *group) {                                      //Initialisation of a function to print the group
+    printGroupLogo(group);                                                          //Prints the group logo
+    listDevelopers(group);                                                          //Lists the developers in the group
+}
+
+void listDevelopers(const DeveloperGroup *group) {                                  //Initialisation of a function to list the developers in the group
+    printf("Developers:\n");                                                        //Prints "Developers:"
+    for (int i = 0; i < group->numDevelopers; i++) {                                //For every developer in the group
+        printDeveloper(&group->developers[i]);                                      //Prints the developer
+    }
+}
